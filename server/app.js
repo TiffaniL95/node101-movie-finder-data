87507c4fa5367120b85540256081bbc55d063ab5
Tiffani <TiffaniL95@gmail.com>
2022-04-06 10:37:57 -0700

    updated code to be more compact

--- server/app.js
+++ server/app.js
@@ -1,57 +1,47 @@
 const express = require('express');
-//establish dependencies
 const morgan = require('morgan');
 const axios = require('axios');
 require('dotenv').config()
 
 
-//create express server
+
 const app = express();
 
-//log each request with morgan dev format
+
 app.use(morgan('dev'))
 
-//create array of objects that will contain movie data
+
 let movieData = [
     
 ]
 
-//create object with i:imdbID and t:Title
-//access object property based on req.url[2]
-//only run one loop
-
-app.get('/', (req, res) => {
+let searchBy = {
+    'i': 'imdbID',
+    't': 'Title'
+    }
     
-
-    if(req.url[2] == 'i'){
+    app.get('/', (req, res) => {
         
-        for(let i = 0; i<movieData.length; i++){
-                
-            if(movieData[i]['imdbID'] == req.query.i){
+    
+        for(let i = 0; i<movieData.length; i++){ 
+
+            let query = searchBy[req.url[2]]
+            let search = req.query.i || req.query.t
+
+                if(movieData[i][query].toLowerCase() == search){
+                    console.log('if statement')
 
                     res.send(movieData[i]);
                     return
+                }
             }
-        }
-
-    } 
     
-    if(req.url[2] == 't') {
-        
-        for(let j = 0; j<movieData.length; j++){           
-
-            if(movieData[j]['Title'].toLowerCase() == req.query.t){
-
-                res.send(movieData[j]);
-                return
-            }
-        }
-    }
         
         axios.get(`http://www.omdbapi.com${req.url}&apikey=${process.env.API_KEY}`) 
         
         .then((response) => {
-            // console.log(response.data)            
+            console.log('API')
+
             let {Title, Year, imdbID} = response.data;
             
             movieData.push({Title, Year, imdbID});
@@ -69,7 +59,6 @@ app.get('*', (req, res) => {
     res.send('Not Found')
 });
 
-//respond to repeat get requests with cached data
 
 
 module.exports = app;
